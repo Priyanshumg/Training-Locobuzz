6a387d5f3d616273f0f4d9ba3d774eaef604aab1
Priyanshumg <ginkalpriyashu@gmail.com>
2024-01-22 20:18:12 +0530

    [Priyanshu] Question 2 Completed

---
+++ Array Problems/Array Problem 2/Array Problem 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Array_Problem_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 5, 3, 7, 5 };
+            //int[] arr = { 1, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine("Unique Values Below");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool duplicate = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        duplicate = true;
+                        break;
+                    }
+                }
+                if (!duplicate)
+                {
+                    Console.WriteLine($"{i}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
