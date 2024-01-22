23f7e869d30f8bd57532663439f3664f96cb6fd6
Priyanshumg <ginkalpriyashu@gmail.com>
2024-01-22 19:07:13 +0530

    [Priyanshu] Added files for Question 1

---
+++ Array Problems/Array Problem 1/Array Problem 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Question:
+Write a program in C# to count total number of duplicate elements in an array
+
+Approach:
+using foreach method to run a for each element in the array
+ */
+
+
+
+namespace Array_Problem_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 3, 4, 5, 6, 6, 7, 8, 9 };
+            foreach (int i in arr)
+            {
+                int count = 0;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] == i)
+                    {
+                        count++;
+                    }
+                }
+                if (count > 1)
+                {
+                    Console.WriteLine($"{i} is repeated {count}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
