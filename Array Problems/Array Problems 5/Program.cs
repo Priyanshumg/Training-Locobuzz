202ef669fdff28acbcc77af2050f97e127491c1a
Priyanshumg <ginkalpriyashu@gmail.com>
2024-01-23 10:21:09 +0530

    [Priyanshu] Question 5 completed

---
+++ Array Problems/Array Problems 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+5) Write a program to print below pattern.
+********
+*******
+******
+*****
+****
+***
+**
+*
+*/
+
+namespace Array_Problems_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = Convert.ToInt32(Console.ReadLine()); i != 0; i--)
+            {
+                string astrix = new string('*', i);
+                Console.WriteLine(astrix);
+            }
+        }
+    }
+}
