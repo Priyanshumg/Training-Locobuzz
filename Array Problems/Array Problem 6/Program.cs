ae1974bb95709e14aa082c5f871b12a287ac60cd
Priyanshumg <ginkalpriyashu@gmail.com>
2024-01-23 11:16:52 +0530

    [Priyanshu] Completed Question 6

---
+++ Array Problems/Array Problem 6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problem_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 7; int cols = 5;
+            for (int i  = 0; i < rows; i++) 
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (i == 0 || i == rows - 1 || j == 0 || j == cols - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
