badcd9f38c54691ff596530ab4e8051369bfdd25
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-01-26 23:43:09 +0530

    [Priyanshu] Done: Completed with the Question 7 and also resolved bugs in the program

---
+++ Array Problems/Array Problems 7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+/*
+Question:
+How to Reverse Each Word in a Given String in C#.
+*/
+namespace Array_Problems_7
+{
+    internal class Program
+    {
+        static string reverse(string WordToReverse)
+        {
+            char[] CharArray = WordToReverse.ToCharArray();
+            Array.Reverse(CharArray);
+            return new string(CharArray);
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] arr= s.Split(' ');
+                foreach (string element in arr)
+                {
+                    string word = reverse(element);
+                    Console.Write($"{word} ");
+                }
+                Console.ReadLine();
+        }
+    }
+}
