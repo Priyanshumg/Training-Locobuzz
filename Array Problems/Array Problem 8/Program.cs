2378e93e2f54627449030f30f2e281f32e2110fe
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-01-27 16:31:44 +0530

    [Priyanshu] Add: Added functionality to sum all the integer value in an array

---
+++ Array Problems/Array Problem 8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Question:
+C# Program to sum up all the digits of a given number
+ */
+namespace Array_Problem_8
+{
+    internal class Program
+    {
+        static int sum_array(int[] array)
+        {
+            int sum_arr=0;
+            foreach (int item in array)
+            {
+                sum_arr += item;
+                Console.WriteLine(item);
+            }
+            return sum_arr;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+            }
+        }
+    }
+}
