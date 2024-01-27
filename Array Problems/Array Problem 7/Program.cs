c5363c41e9c6090e5bd4c4b625dd0fa5eb1994c0
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-01-27 16:26:39 +0530

    [Priyanshu] Add: Added Functionality to sum all the integers

---
+++ Array Problems/Array Problem 7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Question: How to Reverse Each Word in a Given String in C#.
+*/
+
+namespace Array_Probelem_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s = Console.ReadLine();
+            ////char[] a = s.Split;
+            //foreach (char c in s)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+            string s = Console.ReadLine();
+            string a;
+            a = s.Reverse();
+            Console.WriteLine(s);
+            Console.WriteLine($"Reveresed String: {}");
+            Console.ReadLine();
+        }
+    }
+}
